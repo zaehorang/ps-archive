13f0da43bfb2808dde0dc22c92a6598e90b4d752
zaehorang <jaho001@uos.ac.kr>
2026-05-26 14:38:47 +0900

    chore: reorganize algorithm workspace

--- AlgorithmStudy/main.swift
+++
@@ -1,8 +0,0 @@
-//
-//  main.swift
-//  AlgorithmStudy
-//
-//  Created by zaehorang on 4/26/24.
-//
-
-_5597()
---
+++ swift/SwiftAlgorithm/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by zaehorang on 4/26/24.
+//
+
+// Call the solution function you are currently working on here.
+print("D")
