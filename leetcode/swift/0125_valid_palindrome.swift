40b348d285db4b391069d35daa5e4f0889a96ea7
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-06-07 15:55:45 +0900

    solve: leetcode 0125 valid palindrome in swift

--- leetcode/swift/0125_valid_palindrome.swift
+++ leetcode/swift/0125_valid_palindrome.swift
@@ -2,18 +2,7 @@ import Foundation
 
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
-        let new = s.map { 
-            let l = $0.asciiValue!
-
-            if (l >= Character("a").asciiValue! && l <= Character("z").asciiValue!) || 
-            (l >= Character("A").asciiValue! && l <= Character("Z").asciiValue!) ||
-            (l >= Character("0").asciiValue! && l <= Character("9").asciiValue!)
-             {
-                return $0.lowercased()
-            }  
-            return ""
-        }.joined(separator: "")
-
+        let new = s.lowercased().filter { $0.isLetter || $0.isNumber }
         return new == String(new.reversed())
     }
 }
