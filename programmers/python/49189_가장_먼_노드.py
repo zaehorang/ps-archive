a771fd02ad6d6c107d04d9f73e4df2fff1262d61
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-07-07 13:30:32 +0900

    solve: programmers 49189 가장 먼 노드 in python3 #3

--- programmers/python/49189_가장_먼_노드.py
+++ programmers/python/49189_가장_먼_노드.py
@@ -1,45 +1,27 @@
-import heapq
-
-# 2. 다익스트라 알고리즘으로 1번을 시작점으로 한 dist 배열 구하기
-def dijkstra(graph, n):
-    INF = float('inf')
-    
-    dist = [INF for _ in range(n + 1)]
-    dist[1] = 0
-    
-    # (cost, node)
-    heap = [(0, 1)]
-    
-    while heap:
-        current_cost, current_node = heapq.heappop(heap)
-        
-        if dist[current_node] < current_cost:
-            continue
-        
-        for new_node in graph[current_node]:
-            new_cost = current_cost + 1
-            
-            if new_cost < dist[new_node]:
-                dist[new_node] = new_cost   
-                heapq.heappush(heap, (new_cost, new_node))
-    
-    return dist
+from collections import deque
 
 def solution(n, edge):
-    # 1번 노드에서 가장 멀리 떨어진 노드의 개수
-        # 가장 멀리 떨어진 노드: 최단 경로로 이동할 때 간선의 개수가 가장 많은 노드 -> 몇번 건너가냐?
-    # 양방향 그래프
-    # 간선의 cost 같을 때 최단 경로 구하기 -> 다익스트라 알고리즘
     
-    # 1. Graph 만들기
+    # 1. make graph
     graph = [[] for _ in range(n + 1)]
     for a, b in edge:
         graph[a].append(b)
         graph[b].append(a)
-
-    # 3. dist 배열에서 max count return
     
-    dist = dijkstra(graph, n)[1:]
-    target = max(dist)
+    # 2. BFS 알고리즘으로 dist 구하기
+    dist = [-1] * (n + 1)
+    dist[1] = 0
     
-    return dist.count(target)
+    queue =deque([1])
+    
+    while queue:
+        current_node = queue.popleft()
+        
+        for next_node in graph[current_node]:
+            if dist[next_node] == -1:
+                dist[next_node] = dist[current_node] + 1
+                queue.append(next_node)
+    
+    
+    max_dist = max(dist)
+    return dist.count(max_dist)
