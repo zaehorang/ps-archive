6485777c6b23dab4b39394f18bd89e51a7dca7e0
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-08-10 18:29:39 +0900

    solve: programmers 70129 이진 변환 반복하기 in python3 #2

--- programmers/python/70129_이진_변환_반복하기.py
+++ programmers/python/70129_이진_변환_반복하기.py
@@ -1,38 +1,13 @@
-def calculate_binary(n):
-    return format(n, 'b')    
-        
-def remove_zero(str):
-    zero_count = 0
-    original = ""
-    
-    for char in str:
-        if char == "0":
-            zero_count += 1
-        else:
-            original += char
-    return [original, zero_count]
-
 def solution(s):
-    binary_count = 0
+    transform_count = 0
     zero_count = 0
-    
-    copy = s
-    
-    # 1. if s == 1이 아니면, 
-        # 0 제거 -> 0 제거 counting 
-    # 1-1. 1이면 종료
-    
-    while copy != "1":
-        new, n = remove_zero(copy)
+
+    while s != "1":
+        one_count = s.count("1")
+        zero_count += len(s) - one_count
         
-        zero_count += n
-        binary_count += 1
-        
-        copy = calculate_binary(len(new))
-        
-    # 2. 이진 변환 실행: len 계산; l -> s = l (이진수) (반복)
-        # 다시 1로 loop
-    
-    
-    # [이진 변환 횟수, 제거된 0의 개수]
-    return [binary_count, zero_count]
+        s = format(one_count, 'b')
+        # s = bin(one_count)[2:]
+        transform_count += 1
+
+    return [transform_count, zero_count]
