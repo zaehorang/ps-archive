04ccfd55ec4bb7835f1ce98b13736a816fa908d2
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-08-10 18:36:07 +0900

    solve: programmers 70129 이진 변환 반복하기 in python3 #3

--- programmers/python/70129_이진_변환_반복하기.py
+++ programmers/python/70129_이진_변환_반복하기.py
@@ -7,7 +7,25 @@ def solution(s):
         zero_count += len(s) - one_count
         
         s = format(one_count, 'b')
-        # s = bin(one_count)[2:]
+        # s = bin(one_count)[2:] // 0b111
         transform_count += 1
 
     return [transform_count, zero_count]
+
+'''
+binary string 직접 구하는 방식.
+
+10진수 숫자
+   ↓
+2로 나눈다
+   ↓
+나머지(0 또는 1)를 저장한다
+   ↓
+몫을 다시 2로 나눈다
+   ↓
+몫이 0이 될 때까지 반복
+   ↓
+저장한 나머지를 역순으로 읽는다
+   ↓
+2진수 완성
+'''
