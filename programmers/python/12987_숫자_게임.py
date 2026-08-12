45b46b6c7306f189899dd34f844600dc0ee01134
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-08-12 09:57:45 +0900

    solve: programmers 12987 숫자 게임 in python3 #2

--- programmers/python/12987_숫자_게임.py
+++ programmers/python/12987_숫자_게임.py
@@ -8,6 +8,7 @@ def solution(A, B):
     b팀이 가장 높은 점수를 받을 때 점수 return
     
     - a와 b를 비교할 때도 차이가 적게 나서 이겨야 다른 상황에서도 이길 확률이 높다.
+        - 잘못된 그리디 사고! -> 큰 게 큰 걸 이겨야 한다.
     - A의 순서가 의미가 있나? 어차피 b의 순서를 바꾸는거니 A도 정렬해서 비교하는게 더 편할 수도..
     
     B의 가장 큰 수
@@ -20,18 +21,16 @@ def solution(A, B):
     1. A 내림차순 정렬
     2. B도 내림차순 정렬
     3. pointer로 B에서 가장 큰 수가 이길 수 있는 수를 A 배열에서 찾고 인덱스 기록
-    
-    7531
-    6666
+
     '''
     
     A.sort(reverse=True)
     B.sort(reverse=True)
-    
-    point = 0
-    
-    for elem_a in A:
-        if B[point] > elem_a:
-            point += 1
-    
-    return point
+
+    b_idx = 0
+
+    for a in A:
+        if B[b_idx] > a:
+            b_idx += 1
+
+    return b_idx
