913e6d7c7d45c7180656badc3596f39b3a90ee47
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-08-20 09:30:53 +0900

    solve: programmers 42583 다리를 지나는 트럭 in python3 #2

--- programmers/python/42583_다리를_지나는_트럭.py
+++ programmers/python/42583_다리를_지나는_트럭.py
@@ -1,16 +1,6 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    '''
-    10kg 견딘다.
-    트럭은 순차 진행
-    
-    bridge_length: queue length (weight sum limit)
-    bridge -> queue
-    
-    무게 제한을 충족했을 때는 남은 거리를 더해서 빼는게 처리가 빠르긴 할 듯
-    
-    '''
     dq = deque([0] * bridge_length)
     current_weight = 0
     time = 0
@@ -21,28 +11,21 @@ def solution(bridge_length, weight, truck_weights):
         dq.append(weight)
         current_weight += weight
     
-    truck_idx = 0
-    
-    while truck_idx < len(truck_weights):
-        truck_weight = truck_weights[truck_idx]
-        
+    for truck_weight in truck_weights:
         # 트럭 추가 가능하면
         if current_weight + truck_weight <= weight:
             current_weight -= dq.popleft()
-        
             append_truck(truck_weight)
             time += 1
-        else:
-             # 다리 위에 자리 만들기
+        else: # 트럭 추가 불가능하면
+            # 다리 위에 자리 만들기
             while current_weight + truck_weight > weight:
                 time += 1
                 current_weight -= dq.popleft()
                 dq.append(0)
-            dq.pop()
-            append_truck(truck_weight)
             
-        truck_idx += 1
-        
-    # 마지막 트럭 올라가면 위에 반복문 끝
+            dq.pop()  # 임시 0 제거 후 트럭 진입
+            append_truck(truck_weight)
+
     time += bridge_length
     return time
