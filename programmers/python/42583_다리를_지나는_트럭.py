85a9f88f899165d36192a2c4b18651500aaabb16
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-08-20 09:38:18 +0900

    solve: programmers 42583 다리를 지나는 트럭 in python3 #3

--- programmers/python/42583_다리를_지나는_트럭.py
+++ programmers/python/42583_다리를_지나는_트럭.py
@@ -1,31 +1,27 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    dq = deque([0] * bridge_length)
+    current_time = 0
     current_weight = 0
-    time = 0
+    bridge = deque()  # (트럭 무게, 다리를 빠져나가는 시점)
     
-    def append_truck(weight):
-        nonlocal current_weight
-        nonlocal dq
-        dq.append(weight)
-        current_weight += weight
-    
-    for truck_weight in truck_weights:
-        # 트럭 추가 가능하면
-        if current_weight + truck_weight <= weight:
-            current_weight -= dq.popleft()
-            append_truck(truck_weight)
-            time += 1
-        else: # 트럭 추가 불가능하면
-            # 다리 위에 자리 만들기
-            while current_weight + truck_weight > weight:
-                time += 1
-                current_weight -= dq.popleft()
-                dq.append(0)
+    for truck in truck_weights:
+        current_time += 1
+        
+        # 1. 현재 시간에 이미 다리를 다 건넌 트럭들 제거
+        while bridge and bridge[0][1] <= current_time:
+            w, _ = bridge.popleft()
+            current_weight -= w
             
-            dq.pop()  # 임시 0 제거 후 트럭 진입
-            append_truck(truck_weight)
-
-    time += bridge_length
-    return time
+        # 2. 무게 초과로 다음 트럭이 못 올라오면, 앞 트럭이 나갈 때까지 시간 점프
+        while current_weight + truck > weight:
+            w, exit_time = bridge.popleft()
+            current_weight -= w
+            current_time = exit_time  # 앞 트럭이 완전히 빠져나간 시간으로 이동
+            
+        # 3. 새 트럭 진입 (탈출 예정 시간 = 현재 시간 + 다리 길이)
+        bridge.append((truck, current_time + bridge_length))
+        current_weight += truck
+        
+    # 마지막 트럭이 다리를 완전히 빠져나가는 시간이 정답
+    return bridge[-1][1]
