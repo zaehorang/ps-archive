f5441ad0ce98f1e23526e1c7d66a3e361515b21d
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-08-26 11:52:07 +0900

    solve: programmers 49994 방문 길이 in python3 (rev 2)

--- programmers/python/49994_방문_길이.py
+++ programmers/python/49994_방문_길이.py
@@ -1,61 +1,33 @@
 def solution(dirs):
-    '''
-    움직인 길 -> 좌표로 판단할 수 있는지? visited point array
-    - 해당 좌표를 갔는지 판단보다 이전 점에서 해당 좌표로 간 이력이 있는지 판단해야 할 듯
-    
-    같은 시작점, 도착점을 판단 -> 순서가 바껴도 가능
-    
-    - 경계 처리도 해야 함.
-    - 명령어 n <= 500
-    
-    1. 각 좌표를 키로 갖는 dict를 만든다
-    2. 각 좌표로 이동할 때, 시작점, 도착점을 dic에 추가
-        - 양방향으로 표시
-    2-1 이동하면서 해당 좌표 정보가 없으면 경로 + 1, 없으면 0
-    '''
-    
-    visited = {}
-    current = (0, 0)
-    
-    for i in range(-5, 6):
-        for j in range(-5, 6):
-            visited[(i, j)] = set()
-    
-    def move(key):
-        nonlocal current
-        
-        if key == "U":
-            nx = current[0]
-            ny = current[1] + 1 
-            if ny > 5:
-                return
-                
-        elif key == "R":
-            nx = current[0] + 1
-            ny = current[1] 
-            if nx > 5:
-                return
-        elif key == "L":
-            nx = current[0] - 1
-            ny = current[1]
-            if nx < -5:
-                return
-        elif key == "D":
-            nx = current[0]
-            ny = current[1] - 1 
-            if ny < -5:
-                return
-        
-        visited[(current)].add((nx, ny))
-        visited[(nx, ny)].add((current))
-                
-        current = (nx, ny)
-    
-    for direct in dirs:
-        move(direct)
-    
-    answer = 0
-    for s in visited.values():
-        answer += len(s)
-    print(visited)
-    return answer // 2
+    # 방향별 이동량
+    move = {
+        "U": (0, 1),
+        "D": (0, -1),
+        "R": (1, 0),
+        "L": (-1, 0)
+    }
+
+    x, y = 0, 0
+
+    # 방문한 경로(간선)를 저장
+    visited = set()
+
+    for command in dirs:
+        dx, dy = move[command]
+
+        nx = x + dx
+        ny = y + dy
+
+        # 좌표 범위를 벗어나면 이동 자체를 무시
+        if not (-5 <= nx <= 5 and -5 <= ny <= 5):
+            continue
+
+        # 같은 길을 반대 방향으로 지나가는 것도 같은 길이므로
+        # 양방향을 모두 저장
+        visited.add(((x, y), (nx, ny)))
+        visited.add(((nx, ny), (x, y)))
+
+        x, y = nx, ny
+
+    # 하나의 길을 양방향으로 2번 저장했으므로 2로 나눔
+    return len(visited) // 2
