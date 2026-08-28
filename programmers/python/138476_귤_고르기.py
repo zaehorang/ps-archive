f2eccd34a149ccf3c453d6da09b3bf20fc7a3898
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-08-28 09:10:07 +0900

    solve: programmers 138476 귤 고르기 in python3 (rev 2)

--- programmers/python/138476_귤_고르기.py
+++ programmers/python/138476_귤_고르기.py
@@ -1,12 +1,12 @@
 from collections import Counter
 
 def solution(k, tangerine):
-    dict_tangerine = Counter(tangerine)
+    count_tangerine = Counter(tangerine)
 
     answer = 0
     c = k
-    for n in sorted(dict_tangerine.values(), reverse=True):
-        c -= n
+    for _, value in count_tangerine.most_common():
+        c -= value
         answer += 1
         if c <= 0:
             return answer
