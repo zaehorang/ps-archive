b9f3a24a0875f1d921f51e6e13fc4adf6e50fd68
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-09-11 13:47:22 +0900

    solve: programmers 12912 두 정수 사이의 합 in python3 (rev 3)

--- programmers/python/12912_두_정수_사이의_합.py
+++ programmers/python/12912_두_정수_사이의_합.py
@@ -1,5 +1,8 @@
 def solution(a, b):
+    # a가 더 크면 두 값을 교환
+    # Python은 오른쪽 값을 먼저 평가한 뒤 각각 대입하므로 swap 가능
     if a > b:
         a, b = b, a
     
-    return sum(range(a, b+1))
+    # a부터 b까지의 합
+    return sum(range(a, b + 1))
