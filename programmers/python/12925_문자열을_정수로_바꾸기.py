69b442a1658d14fc4d607a4c073e261a51cf4557
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-09-14 08:47:41 +0900

    solve: programmers 12925 문자열을 정수로 바꾸기 in python3 (rev 2)

--- programmers/python/12925_문자열을_정수로_바꾸기.py
+++ programmers/python/12925_문자열을_정수로_바꾸기.py
@@ -1,7 +1,3 @@
 def solution(s):
-    if s[0] == "-":
-        return -int(s[1:])
-    elif s[0] == "+":
-        return int(s[1:])
-    
+    # str -> int 형변환 시에 +, -는 알아서 처리 된다.
     return int(s)
