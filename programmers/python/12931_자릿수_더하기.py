1a5338ed8353dfdb0ac9ff41a0d46487c739337c
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-09-17 17:34:51 +0900

    solve: programmers 12931 자릿수 더하기 in python3 (rev 2)

--- programmers/python/12931_자릿수_더하기.py
+++ programmers/python/12931_자릿수_더하기.py
@@ -2,8 +2,8 @@ def solution(n):
     answer = 0
     
     while n != 0:
-        remainder = int(n % 10)
-        n /= 10
+        remainder = n % 10
+        n //= 10
         answer += remainder
 
     return answer
