bfa0aa3d3a1d449490acc54c79b0c68b24b00978
Jaeho Lee <123877353+zaehorang@users.noreply.github.com>
2026-09-17 17:44:37 +0900

    solve: programmers 12928 약수의 합 in python3 (rev 2)

--- programmers/python/12928_약수의_합.py
+++ programmers/python/12928_약수의_합.py
@@ -1,8 +1,2 @@
 def solution(n):
-    answer = n
-    
-    for i in range(1, n // 2 + 1):
-        if n % i == 0:
-            answer += i
-        
-    return answer
+    return n + sum([i for i in range(1, (n // 2 + 1)) if n % i == 0])
